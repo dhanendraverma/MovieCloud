a694a0a381c9a53277df86dcaa18ca948688136d
unknown <v.dhanendra@gmail.com>
2019-09-03 16:48:48 +0530

    add navbar

--- MovieCloud/urls.py
+++ MovieCloud/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # re_path(r'test/',views.call_script),
-    re_path('^$',),
+    # re_path('^$',),
     re_path('', include('movie.urls')),
 ]
--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, re_path
 from . import views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    re_path(r'test2/', views.movie_view)
+    re_path(r'test2/', views.movie_view),
+    re_path(r'test3/', TemplateView.as_view(template_name="index.html")),
     ]
