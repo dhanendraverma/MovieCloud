80cb2626a67befb25deed2fae006f74170409c89
G.Venkatesh <garnepudi.venkatesh@gmail.com>
2019-09-09 23:17:31 +0530

    added chart, should work on inputs to chart. change views.py code for necessary input to report.html

--- movie/urls.py
+++ movie/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	re_path(r'movie/$',views.MovieListView.as_view(),name='movies'),
 	re_path(r'create/$', views.MovieCreatView.as_view(), name='movie-add'),
 	re_path(r'test2/', views.movie_view),
-    re_path(r'show_(?P<name>\d+)/',views.handle),
+    re_path(r'show_(?P<name>[a-zA-Z#]+)/',views.handle, name = 'movie_chart'),
     re_path(r'^$', views.MovieListView.as_view(),name='movie-list')
 
     ]
--- movie/views.py
+++ movie/views.py
@@ -1,26 +1,26 @@
-from django.http import HttpResponse 
-from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .forms import *
 from django.views.generic.edit import CreateView
 from django.views import generic
 from movie.models import Movie
 from django.urls import reverse_lazy, reverse
-
+import pandas as pd
 
 def movie_view(request):
-    if request.method == 'POST': 
-        form = movieForm(request.POST, request.FILES) 
-  
-        if form.is_valid(): 
-            form.save() 
-            return redirect('success') 
-    else: 
-        form = movieForm() 
-    return render(request, 'sample.html', {'form' : form}) 
-  
-  
-def success(request): 
-    return HttpResponse('successfuly uploaded') 
+    if request.method == 'POST':
+        form = movieForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = movieForm()
+    return render(request, 'sample.html', {'form' : form})
+
+
+def success(request):
+    return HttpResponse('successfuly uploaded')
 
 
 
@@ -41,7 +41,14 @@ class MovieCreatView(CreateView):
 
 
 def handle(request,**kwargs):
-    for key, value in kwargs.items(): 
-        print ("%s == %s" %(key, value)) 
+    for key, value in kwargs.items():
+        print ("%s == %s" %(key, value))
     print(request)
-    return HttpResponse('reaching here!!')
+    movie_object = Movie.objects.get(movie_name = "#"+value)
+    file_path = "/home/venkatesh/Desktop/MovieCloud/media/Excel/#"+value+".xlsx"
+
+    mov = pd.read_excel(file_path)
+    print(mov.head())
+    context = {'movie':movie_object}
+    print(context)
+    return render(request, 'movie/report.html', context)
